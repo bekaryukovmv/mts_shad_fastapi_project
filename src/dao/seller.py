cfa53c0022e81ff32dfe0f9becc96df07cf36153
Святослав <swja7ik@mail.ru>
2024-03-01 02:42:54 +0300

    refactor seller

--- src/dao/seller.py
+++ src/dao/seller.py
@@ -1,3 +1,6 @@
+from sqlalchemy import select
+
+from src.configurations import get_async_session
 from src.dao.base import BaseDAO
 from src.models.books import Seller
 
--- src/routers/v1/seller.py
+++ src/routers/v1/seller.py
@@ -6,7 +6,9 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.configurations.database import get_async_session
+from src.lib.auth import hash_password, current_seller
 from src.models.books import Book
+from src.models.seller import Seller
 from src.schemas import IncomingBook, ReturnedAllBooks, ReturnedBook
 from src.dao import SellerDAO
 from src.schemas.seller import SellerAuth
@@ -18,9 +20,18 @@ DBSession = Annotated[AsyncSession, Depends(get_async_session)]
 
 
 @seller_router.post("/")
-async def register_seller(seller_data: SellerAuth):
-    user = await SellerDAO.find_one_or_none(email=seller_data.email)
-    if user:
+async def register_seller(seller_auth: SellerAuth):
+    seller = await SellerDAO.find_one_or_none(email=seller_auth.email)
+    if seller:
         # Пользователь существует
         raise HTTPException(409, "Пользователь с таким email уже зарегистрирован")
-    hashed_password = get_password_hash(seller_data.password)
+    hashed_password = hash_password(seller_auth.password)
+    await SellerAuth.add(email=seller_auth.email, password=hashed_password)
+    return Response(status_code=status.HTTP_201_CREATED)
+
+
+@seller_router.get("/")
+async def get_all_sellers(seller: Seller = Depends(current_seller)):
+    sellers = await SellerDAO.find_all()
+    return sellers
+
