206f932f5a54304eb64f54256930325ca09027ed
tarit666 <79465997+alextarit@users.noreply.github.com>
2024-03-03 13:54:07 +0300

    merge with the old repository was performed

--- src/models/books.py
+++ src/models/books.py
@@ -1,4 +1,5 @@
 from sqlalchemy import String
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import BaseModel
@@ -7,6 +8,7 @@ from .base import BaseModel
 class Book(BaseModel):
     __tablename__ = "books_table"
 
+    parent_id: Mapped[int] = mapped_column(ForeignKey("sellers_table.id"))
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(50), nullable=False)
     author: Mapped[str] = mapped_column(String(100), nullable=False)
--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,3 +1,4 @@
 from .books import *  # noqa F403
+from .sellers import * # noqa F403
 
-__all__ = books.__all__  # noqa F405
+__all__ = books.__all__ and sellers.__all__ # noqa F405
